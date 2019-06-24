1a8a600e34a4dee7b2d8574aab21bb0119cc420e
WillyRL <willyrobertus@gmail.com>
2019-06-24 22:30:19 +0700

    Create idch-nx-installer.sh

---
+++ idch-nx-installer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "IDCloudHost Exim Plugins Installer"
+echo "Get Nixstats Plugins Folder.. "
+nxplugindir=$(nixstatsagent --info | awk 'NR == 3 {print $3}')
+echo "Nixstats plugins folder Results : $nxplugindir"
+echo "Clear Old plugins file"
+rm -rf $nxplugindir/idch-exim.py
+echo "Success.."
+echo "Downloading Nixstats Custom Plugins from git repository"
+wget https://raw.githubusercontent.com/WillyRL/Nixstats/master/idch-exim.py -O $nxplugindir/idch-exim.py
+echo "Append Config to /etc/nixstats.ini in .. 3  "
+echo "Append Config to /etc/nixstats.ini in .. 2  "
+echo "Append Config to /etc/nixstats.ini in .. 1  "
+if grep -R "idch-exim" /etc/nixstats.ini 
+then
+	echo "idch-exim Exists in nixstats config.. skipping enable idch-exim"
+else
+	echo -e "[idch-exim]\nenabled = yes" >> /etc/nixstats.ini
+fi
+echo "Done.. New /etc/nixstats.ini showing ..."
+cat /etc/nixstats.ini
+echo -R "\n \n \n \n "
+echo "Testing Plugin.. Output showing ..."
+nixstatsagent test idch-exim
+echo "All Setup Done .. Restarting nixstats agent ..."
+service nixstatsagent restart
+echo "Complete.. Script terminating ..."
