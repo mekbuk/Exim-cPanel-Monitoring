11d4348b20baca89d85f248e2b716451060095d8
Willy Robertus Leonardo <willyrobertusleonardo@System.local>
2019-12-02 01:39:31 +0700

    Menambahkan perhitungan rata rata memory kosong per hari dengan sample data tertentu

--- sar-report.sh
+++ sar-report.sh
@@ -4,6 +4,9 @@
 # RnD - Willy Robertus Leonardo
 
 
+
+##CPU
+echo "+------------------------------------CPU INFORMATION--------------------------------"
 echo "+----------------------------------------------------------------------------------+"
 echo "|Average:         CPU     %user     %nice   %system   %iowait    %steal     %idle  |"
 echo "+----------------------------------------------------------------------------------+"
@@ -22,9 +25,9 @@ for file in `ls -tr /var/log/sa/sa* | grep -v sar`
 do
 
 count=$((count+1))
-dat=`sar -f $file | head -n 1 | awk '{print $4}'`
+datCpu=`sar -f $file | head -n 1 | awk '{print $4}'`
 
-echo -n $dat
+echo -n $datCpu
 sar -f $file  | grep -i Average | sed "s/Average://"
 userArr+=(`sar -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $2}'`)
 niceArr+=(`sar -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $3}'`)
@@ -35,7 +38,50 @@ idleArr+=(`sar -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{p
 
 done
 
+##Memory
+echo "+----------------------------------MEMORY INFORMATION------------------------------------------------------------+"
+echo "+----------------------------------------------------------------------------------------------------------------+"
+echo "|Average:     kbmemfree kbmemused  %memused kbbuffers  kbcached  kbcommit   %commit  kbactive   kbinact   kbdirty|"
+echo "+----------------------------------------------------------------------------------------------------------------+"
+
+
+
+memfreeArr=()
+memusedArr=()
+memusedpArr=()
+buffersArr=()
+cachedrArr=()
+commitArr=()
+commitpArr=()
+activeArr=()
+inactArr+=()
+dirtyArr+=()
+
+for file in `ls -tr /var/log/sa/sa* | grep -v sar`
+
+do
+
+datMemory=`sar -f $file | head -n 1 | awk '{print $4}'`
+echo -n $datMemory
+sar -r -f $file  | grep -i Average | sed "s/Average://"
+memfreeArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $1}'`)
+memusedArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $2}'`)
+memusedpArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $3}'`)
+buffersArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $4}'`)
+cachedArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $5}'`)
+commitArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $6}'`)
+commitpArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $7}'`)
+activeArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $8}'`)
+inactArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $9}'`)
+dirtyArr+=(`sar -r -f $file  | grep -i Average | sed "s/Average://" | awk -F ' ' '{print $10}'`)
+
+done
+
+#Operasi perhitungan
 echo "+----------------------------------------------------------------------------------+"
+
+konversiGB="1048576"
+#CPU Section
 sumUser=$( IFS="+"; bc <<< "${userArr[*]}" )
 sumNice=$( IFS="+"; bc <<< "${niceArr[*]}" )
 sumSystem=$( IFS="+"; bc <<< "${systemArr[*]}" )
@@ -51,10 +97,27 @@ avgIowait=`echo "scale=2; $sumIowait/$count" | bc`
 avgSteal=`echo "scale=2; $sumSteal/$count" | bc`
 avgIdle=`echo "scale=2; $sumIdle/$count" | bc`
 
+#Memory Section
+sumFreem=$( IFS="+"; bc <<< "${memfreeArr[*]}" )
+sumbuffer=$( IFS="+"; bc <<< "${buffersArr[*]}" )
+sumcached=$( IFS="+"; bc <<< "${cachedArr[*]}" )
 
+
+totalfreem=$(expr $sumFreem + $sumbuffer + $sumcached)
+avgfreem=`echo "scale=2; $totalfreem/$count" | bc`
+
+avgfreem=`echo "scale=2; $avgfreem/$konversiGB" | bc`
+
+
+
+echo "Total Sample Data = " $count " Hari"
+echo "CPU Results : "
 echo "Average Monthly %user : " $avgUser
 echo "Average Monthly %Nice : " $avgNice
 echo "Average Monthly %System : " $avgSystem
 echo "Average Monthly %IOWait : " $avgIowait
 echo "Average Monthly %Steal : " $avgSteal
-echo "Average Monthly %Idle : " $avgIdle
+echo "Average Monthly %Idle : " $avgIdle 
+
+echo "Memory Results :"
+echo "Free Memory Average   : " $avgfreem "GB"
